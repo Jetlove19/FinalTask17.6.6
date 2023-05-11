7081b4652c3126d44b97e4516da5689d35d5bf8c
Jetlove19 <rbrorlove@gmail.com>
2023-05-11 13:53:28 +0300

    Добавьте файлы проекта.

---
+++ FinalTask17.6.6/Account.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask17._6._6
+{
+
+        public class Account : IUsually, ISalary
+        {
+            // тип учетной записи
+            public string Type { get; set; }
+
+            // баланс учетной записи
+            public double Balance { get; set; }
+
+            // процентная ставка
+            public double Interest { get; set; }
+
+            // методы дляпрофилей
+            void IUsually.CalculateInterest()
+            {
+                Interest = Balance * 0.4;
+
+                if (Balance < 1000)
+                    Interest -= Balance * 0.2;
+
+                if (Balance < 50000)
+                    Interest -= Balance * 0.4;
+            }
+            void ISalary.CalculateInterest()
+            {
+                Interest = Balance * 0.5;
+            }
+        }
+}
---
+++ FinalTask17.6.6/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask17._6._6
+{
+    public static class Calculator
+    {
+        // Метод для расчета процентной ставки
+        public static void CalculateInterest(Account account)
+        {
+            Console.WriteLine(account.Interest);
+        }
+    }
+}
